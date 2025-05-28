265ed2782428973e8e8119dc5bdb0b3501c8fe55
Rodrigo Sardinha <rodrigosardinha@gmail.com>
2025-05-28 10:33:32 -0300

    Ajustes

--- clean_pod.sh
+++ clean_pod.sh
@@ -7,6 +7,14 @@ read -p "Digite o nome do pod: " POD_NAME
 # Caminho do diretório a ser limpo no pod
 DIRETORIO_NO_POD="/opt/silfaepub-docs"
 
+echo "ATENÇÃO: Todos os arquivos no diretório $DIRETORIO_NO_POD serão permanentemente apagados e não poderão ser recuperados!"
+read -p "Tem certeza que deseja continuar? (s/N): " CONFIRMACAO
+
+if [[ "$CONFIRMACAO" != "s" && "$CONFIRMACAO" != "S" ]]; then
+    echo "Operação cancelada pelo usuário."
+    exit 1
+fi
+
 echo "Iniciando limpeza dos arquivos no diretório $DIRETORIO_NO_POD no pod $POD_NAME no namespace $NAMESPACE..."
 
 # Executar comando para remover apenas os arquivos, mantendo as pastas
